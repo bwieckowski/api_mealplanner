bc8d8750fda6473f4b20b4c61e2f74de25f4f8e2
Smatix <mateusz.suchenia@interia.pl>
2018-12-29 14:23:37 +0100

    Refactor exception handler

--- src/Event/ExceptionSubscriber.php
+++ src/Event/ExceptionSubscriber.php
@@ -3,11 +3,11 @@
 namespace App\Event;
 
 use App\Exception\ApiExceptionInterface;
+use App\Factory\ErrorResponseFactory;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionSubscriber implements EventSubscriberInterface
@@ -15,23 +15,17 @@ class ExceptionSubscriber implements EventSubscriberInterface
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $e = $event->getException();
-        if (!$event->getException() instanceof ApiExceptionInterface) {
-            $statusCode = $e instanceof HttpExceptionInterface ? $e->getStatusCode() : 500;
-            $data = $this->buildResponseData(Response::$statusTexts[$statusCode],$statusCode);
-            $response = new JsonResponse($data, $statusCode);
+        $factory = new ErrorResponseFactory();
+        if ($e instanceof AuthenticationException) {
+            $response = $factory->create($e->getMessageKey(),401);
+        } else if (!$e instanceof ApiExceptionInterface) {
+            return;
+//            $statusCode = $e instanceof HttpExceptionInterface ? $e->getStatusCode() : 500;
+//            $response = $factory->create(Response::$statusTexts[$statusCode],$statusCode);
         } else {
-            $data = $this->buildResponseData($e->getMessage(),$e->getStatusCode());
-            $response = new JsonResponse($data, $e->getStatusCode());
+            $response = $factory->create($e->getMessage(),$e->getCode());
         }
-        $response->headers->set('Content-Type', 'application/problem+json');
         $event->setResponse($response);
-    }
-    public function buildResponseData($message,$code)
-    {
-        return [
-            'code' => $code,
-            'messages' => $message
-        ];
     }
     public static function getSubscribedEvents()
     {
